216274e2d1b56fc85108d0fce3956d35e9d4df4b
Çetin Utku Serdar <cetinutkuserdar@Mac.TCCB.GOV.TR>
2025-12-29 20:14:52 +0300

    docs - add screenshot to README

--- fwe.py
+++
@@ -1,6 +0,0 @@
-def get_topics(topic_count):
-    topics = []
-    for i in range(1,topic_count + 1):
-        all_topics = input("Lütfen konu adını giriniz: ")
-        topics.append(all_topics)
-    return topics
